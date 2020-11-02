389183f194102de09f58e9a9a8a0e0d8f1270d76
Bruno Rondon da Silva <bruno.engmeca@gmail.com>
2020-11-02 08:53:02 -0400

    Inserindo tratamento de erros na api.

--- ProAgil.API/Controllers/ValuesController.cs
+++ ProAgil.API/Controllers/ValuesController.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using ProAgil.API.Data;
 using ProAgil.API.Model;
 
@@ -22,16 +24,35 @@ namespace ProAgil.API.Controllers
 
         // GET api/values
         [HttpGet]
-        public ActionResult<IEnumerable<Evento>> Get()
+        public async Task<ActionResult> Get()
         {
-            return _context.Eventos.ToList();
+            try
+            {
+                var results = await _context.Eventos.ToListAsync();
+                return Ok(results);
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError,"Banco de Dados Falhou.");
+            }            
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public ActionResult<Evento> Get(int id)
+        public async Task<ActionResult> Get(int id)
         {
-            return _context.Eventos.FirstOrDefault(x => x.EventoId == id);
+            try
+            {
+                var results = await _context.Eventos.FirstOrDefaultAsync(x => x.EventoId == id);
+                
+                return Ok(results);
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Banco de Dados Falhou.");
+            }
+
+            
         }
 
         // POST api/values
